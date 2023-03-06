ff686397a508bdb497b508ac936f02e7b1460517
Silvia Lupsa <lupsasilvia@gmail.com>
2023-03-06 14:02:12 +0200

    add pizza and allergens

---
+++ backend/allergens.js
@@ -0,0 +1,38 @@
+const allergens = [
+    {
+        "id": 1,
+        "name": "gluten"
+    }, 
+    {
+        "id": 2,
+        "name": "crustaceans"
+    },
+    {
+        "id": 3,
+        "name": "eggs"
+    },
+    {
+        "id": 4,
+        "name": "fish"
+    },
+    {
+        "id": 5,
+        "name": "peanuts"
+    },
+    {
+        "id": 6,
+        "name": "soybeans"
+    },
+    {
+        "id": 7,
+        "name": "milk"
+    },
+    {
+        "id": 8,
+        "name": "nuts"
+    },
+    {
+        "id": 9,
+        "name": "mustard"
+    }
+]
