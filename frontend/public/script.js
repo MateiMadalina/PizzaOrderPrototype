5bdb9925ed1cb875a8c504dd87ab870d5392dc66
Your Name <sinziana.babu@yahoo.com>
2023-03-07 19:46:20 +0200

    add arr

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -35,47 +35,50 @@ const allergens = async () => {
 const data = async () => {
   const databasePizza = await pizza();
   const databaseAllergens = await allergens();
-  console.log(allergensArr);
-    // if (allergensArr.length === 0) {
-    const pizzaMap = databasePizza.map((element) => {
-      const allergensMap = databaseAllergens.map((allergen) => {
-        const idAllergens = element.allergens.map((id) => {
-          return id === allergen.id
-            ? `<li id="allergen">${allergen.name}</li>`
-            : null;
-        });
+  const pizzaMap = databasePizza.map((element) => {
+    const allergensMap = databaseAllergens.map((allergen) => {
+      const idAllergens = element.allergens.map((id) => {
+        return id === allergen.id
+          ? `<li id="allergen">${allergen.name}</li>`
+          : null;
+      });
 
-        return idAllergens.join("");
-      });
-      const ingredientsArray = element.ingredients.map((ingredient) => {
-        return `<li id="ingredient">${ingredient}</li>`;
-      });
-      return `<h3 id="pizzaName">${element.name}</h3>
+      return idAllergens.join("");
+    });
+
+    const ingredientsArray = element.ingredients.map((ingredient) => {
+      return `<li id="ingredient">${ingredient}</li>`;
+    });
+    return `<h3 id="pizzaName">${element.name}</h3>
             <img src="${element.photo}">
             <h5>Ingredients:</h5>
             <ul id="allIngredients">${ingredientsArray.join("")}</ul>
             <h5 id="price">Price: ${element.price} €</h5>
             <h5>Allergens</h5>
             <h6 id="allAlergens">${allergensMap.join("")}</h6> `;
-    });
-    if (allergensArr.length === 0) {
-            menu.innerHTML=""
-               menu.insertAdjacentHTML("beforeend", pizzaMap.join(""));
-        } else {
-            menu.innerHTML=""
-            let a =[]
-               allergensArr.map((item) => {
-                 pizzaMap.forEach((el, i, arr) => {
-                   if (!arr[i].includes(item)) {
-                     a.push(arr[i]);
-                   }
-                 });
-               });
-               menu.insertAdjacentHTML("beforeend", a.join(""));
-        }
+  });
+  if (allergensArr.length === 0) {
+    menu.innerHTML = "";
+    menu.insertAdjacentHTML("beforeend", pizzaMap.join(""));
+  } else {
+    menu.innerHTML = "";
+       const filteredPizzas = pizzaMap.filter((pizza) => {
+         const allergensList = pizza.match(/<li id="allergen">(.*?)<\/li>/g);
+         for (let i = 0; i < allergensList.length; i++) {
+           const allergen = allergensList[i].match(
+             /<li id="allergen">(.*?)<\/li>/
+           )[1];
+           if (allergensArr.includes(allergen)) {
+             return false;
+           }
+         }
+         return true;
+       });
 
+    menu.insertAdjacentHTML("beforeend", filteredPizzas.join(""));
+  }
+};
 
-}
 data();
 
 const createOptions = async (arr) => {
@@ -85,53 +88,59 @@ const createOptions = async (arr) => {
 };
 
 const deleteAllergens = () => {
-    const buttonX = document.querySelectorAll(".buttonAll");
-    const arrayBtnX = [...buttonX];
-    arrayBtnX.forEach((button) => {
-      if (!button.hasEventListener) {
-        button.hasEventListener = true;
-        button.addEventListener("click", (e) => {
+  const buttonX = document.querySelectorAll(".buttonAll");
+  const arrayBtnX = [...buttonX];
+  arrayBtnX.forEach((button) => {
+    if (!button.hasEventListener) {
+      button.hasEventListener = true;
+      button.addEventListener("click", (e) => {
+      const allergenName =
+        e.target.parentElement.querySelector("p").textContent;
+      const allergenIndex = allergensArr.indexOf(allergenName);
+          allergensArr.splice(allergenIndex,1)
+          console.log(allergenIndex);
           e.target.parentElement.remove();
-        });
-      }
-    });
-  };
 
-  inputAllergens.addEventListener("input", async () => {
-      const databaseAllergens = await allergens();
-      
-    datalistAlg.innerHTML = "";
-    if (inputAllergens.value.length > 0) {
-      let allergenNames = databaseAllergens.filter((elem) =>
-        elem.name.includes(inputAllergens.value)
-      );
-      createOptions(allergenNames);
-      allergenNames.map((elem) => {
-        if (detailsAllergens.innerHTML.includes(elem.name)) {
-            if (elem.name === inputAllergens.value) {
-              allergensArr.push(elem.name)
-            detailsAllergens.insertAdjacentHTML(
-              "afterbegin",
-              `
+      });
+    }
+  });
+};
+
+inputAllergens.addEventListener("input", async () => {
+  const databaseAllergens = await allergens();
+
+  datalistAlg.innerHTML = "";
+  if (inputAllergens.value.length > 0) {
+    let allergenNames = databaseAllergens.filter((elem) =>
+      elem.name.includes(inputAllergens.value)
+    );
+    createOptions(allergenNames);
+    allergenNames.map((elem) => {
+      if (detailsAllergens.innerHTML.includes(elem.name)) {
+        if (elem.name === inputAllergens.value) {
+          allergensArr.push(elem.name);
+          detailsAllergens.insertAdjacentHTML(
+            "afterbegin",
+            `
               <div class="row">
               <button class="buttonAll">X</button>
               <p>${elem.name}</p>
               </div>
               `
-            );
-            inputAllergens.value = "";
-          }
+          );
+          inputAllergens.value = "";
         }
-      });
+      }
+    });
     //   const arr = [...detailsAllergens.querySelectorAll(".row")];
     //   arr.forEach((row, i, arr) => {
     //   if (!arr.slice(0, i).innerText.includes(row.innerText))
     //      row.remove();
     //   });
     }
-      deleteAllergens();
-      data();
-  });
-
+             console.log(allergensArr);
+  deleteAllergens();
+  data();
+});
 
 //fac POST cu id-ul de la pizza pentru acel order
