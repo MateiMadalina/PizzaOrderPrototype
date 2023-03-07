8ebde37f52924375ace13a74650ae0dc054e1eb6
Silvia Lupsa <lupsasilvia@gmail.com>
2023-03-07 13:56:16 +0200

    insert details

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { dirname } = require("path");
 const path = require("path");
-const pizzaRoutes = require("./routes/routes");
-const { allergens } = require("./allergens.js");
+const pizzaRoutes = require("./routes/pizza");
+const allergens  = require("./routes/allergens");
 
 const port = 9002;
 
@@ -10,16 +10,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 app.use("/api/pizza", pizzaRoutes);
+app.use("/api/allergen", allergens);
 
 app.get(["/", "/order/pizza"], (req, res) =>
     res.sendFile(path.resolve(__dirname + "/../frontend/index.html"))
 );
 app.use("/public",express.static(path.resolve(`${__dirname}/../frontend/public`)));
-
-app.get("/api/allergen", (req, res) => {
-    res.send(allergens)
-})
 
 app.listen(port, (_) => console.log(`http://127.0.0.1:${port}/order/pizza`));
