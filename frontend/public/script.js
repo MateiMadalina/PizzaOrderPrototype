0eb2ad96412f6f44f4449609a3fc47efc4979895
Your Name <sinziana.babu@yahoo.com>
2023-03-07 17:33:57 +0200

    add arr

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -20,7 +20,7 @@ rootEl.insertAdjacentHTML(
 const datalistAlg = document.getElementById("alg");
 const inputAllergens = document.getElementById("allergensSearch");
 const detailsAllergens = document.getElementById("detailsAllergens");
-
+const menu = document.getElementById("menu");
 const pizza = async () => {
   const response = await fetch("http://127.0.0.1:9002/api/pizza");
   const allPizza = await response.json();
@@ -36,7 +36,7 @@ const data = async () => {
   const databasePizza = await pizza();
   const databaseAllergens = await allergens();
   console.log(allergensArr);
-  if (allergensArr.length === 0) {
+    // if (allergensArr.length === 0) {
     const pizzaMap = databasePizza.map((element) => {
       const allergensMap = databaseAllergens.map((allergen) => {
         const idAllergens = element.allergens.map((id) => {
@@ -58,45 +58,22 @@ const data = async () => {
             <h5>Allergens</h5>
             <h6 id="allAlergens">${allergensMap.join("")}</h6> `;
     });
-    document
-      .querySelector("#menu")
-      .insertAdjacentHTML("beforeend", pizzaMap.join(""));
-  } else {
-    while (document.querySelector("#menu").firstChild) {
-        document.querySelector("#menu").removeChild(document.querySelector("#menu").firstChild);
-    }
-    const pizzaMap = databasePizza.map((element) => {
-        const allergensMap = databaseAllergens.map((allergen) => {
-          const idAllergens = element.allergens.map((id) => {
-            return id === allergen.id
-              ? `<li id="allergen">${allergen.name}</li>`
-              : null;
-          });
-  
-          return idAllergens.join("");
-        });
-        const ingredientsArray = element.ingredients.map((ingredient) => {
-          return `<li id="ingredient">${ingredient}</li>`;
-        });
-        return `<h3 id="pizzaName">${element.name}</h3>
-              <img src="${element.photo}">
-              <h5>Ingredients:</h5>
-              <ul id="allIngredients">${ingredientsArray.join("")}</ul>
-              <h5 id="price">Price: ${element.price} €</h5>
-              <h5>Allergens</h5>
-              <h6 id="allAlergens">${allergensMap.join("")}</h6> `;
-      });
-       console.log(pizzaMap[0])
-       let a = [];
-      allergensArr.map(item => {
-        pizzaMap.forEach((el,i,arr)=>{
-            if(!arr[i].includes(item)){
-               a.push(arr[i])
-            }
-        })
-      })
-      document.querySelector("#pizzaList").insertAdjacentHTML("beforeend", a.join(""));
-  }
+    if (allergensArr.length === 0) {
+            menu.innerHTML=""
+               menu.insertAdjacentHTML("beforeend", pizzaMap.join(""));
+        } else {
+            menu.innerHTML=""
+            let a =[]
+               allergensArr.map((item) => {
+                 pizzaMap.forEach((el, i, arr) => {
+                   if (!arr[i].includes(item)) {
+                     a.push(arr[i]);
+                   }
+                 });
+               });
+               menu.insertAdjacentHTML("beforeend", a.join(""));
+        }
+
 
 }
 data();
