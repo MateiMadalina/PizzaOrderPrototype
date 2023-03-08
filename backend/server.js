ad8ccd8678b03f4ff521255e8b2506384f2a3955
Your Name <sinziana.babu@yahoo.com>
2023-03-08 12:14:39 +0200

    Add route to orders

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { dirname } = require("path");
 const path = require("path");
 const pizzaRoutes = require("./routes/pizza");
 const allergens  = require("./routes/allergens");
-
+const orders = require("./routes/orders")
 const port = 9002;
 
 const app = express();
@@ -12,10 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/pizza", pizzaRoutes);
 app.use("/api/allergen", allergens);
-
+app.use("/api/orders", orders)
 app.get(["/", "/pizza/list"], (req, res) =>
     res.sendFile(path.resolve(__dirname + "/../frontend/index.html"))
 );
+
+
+
 app.use("/public",express.static(path.resolve(`${__dirname}/../frontend/public`)));
 
 app.listen(port, (_) => console.log(`http://127.0.0.1:${port}/pizza/list`));
